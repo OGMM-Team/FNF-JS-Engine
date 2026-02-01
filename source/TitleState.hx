b555419490e67a9ddc7f305b9a6c25b6f1e0627c
OfficialGamer <140297634+Official3Gamer@users.noreply.github.com>
2026-02-01 14:52:48 -0600

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -209,17 +209,21 @@ class TitleState extends MusicBeatState
     logoBl.animation.addByPrefix('bump', 'logo bumpin', 24, false);
     logoBl.animation.play('bump');
     logoBl.updateHitbox();
+    // center logo horizontally
+    logoBl.screenCenter(X);
 
     swagShader = new ColorSwap();
-    gfDance = new FlxSprite(titleJSON.gfx, titleJSON.gfy);
-
-    gfDance.frames = Paths.getSparrowAtlas('gfDanceTitle');
-    gfDance.animation.addByIndices('danceLeft', 'gfDance', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
-    gfDance.animation.addByIndices('danceRight', 'gfDance', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
-    gfDance.antialiasing = ClientPrefs.globalAntialiasing;
-
-    add(gfDance);
-    gfDance.shader = swagShader.shader;
+    // gfDance removed: disable loading of gfDanceTitle asset
+    // gfDance = new FlxSprite(titleJSON.gfx, titleJSON.gfy);
+    //
+    // gfDance.frames = Paths.getSparrowAtlas('gfDanceTitle');
+    // gfDance.animation.addByIndices('danceLeft', 'gfDance', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
+    // gfDance.animation.addByIndices('danceRight', 'gfDance', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
+    // gfDance.antialiasing = ClientPrefs.globalAntialiasing;
+    //
+    // do not add gfDance to the state or set shader to avoid using gfDanceTitle asset
+    // add(gfDance);
+    // gfDance.shader = swagShader.shader;
     add(logoBl);
     logoBl.shader = swagShader.shader;
 
